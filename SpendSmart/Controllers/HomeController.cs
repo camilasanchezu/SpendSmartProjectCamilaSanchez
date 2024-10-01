874addd848eead6c1f525b489d0e88f7ffdb0e4b
Camila Isabel Sánchez <camila.sanchez.urresta@udla.edu.ec>
2024-10-01 18:29:36 -0500

    Agregar archivos de proyecto.

---
+++ SpendSmart/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+using System.Diagnostics;
+
+namespace SpendSmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly SpendSmartDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Expenses()
+        {
+            var allExpenses = _context.Expenses.ToList();
+            var totalExpenses = allExpenses.Sum(x => x.Value);
+
+            ViewBag.Expenses = totalExpenses;
+
+            return View(allExpenses);
+        }
+        public IActionResult CreateExpense(int? id)
+        {
+            if(id !=null)
+            {
+                //editar, cargar un un gasto por id
+                var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+                return View(expenseInDb);
+            }
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult CreateExpenseForm(Expense model)
+        {
+            if(model.Id == 0)
+            {
+                //crear
+                _context.Expenses.Add(model);
+
+            }
+            else
+            {
+                //editar
+                _context.Expenses.Update(model);
+            }
+            
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Expenses");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
